869598674c7a10c68db9906fc1cf8b0e2e7869f9
Tomas Frongillo <38133977+tomifrongi@users.noreply.github.com>
2024-02-20 13:46:51 -0300

    quick fix

--- movie-base/app/models/film.rb
+++ movie-base/app/models/film.rb
@@ -16,7 +16,7 @@ class Film < ApplicationRecord
   has_one :source
 
   validates :title, :genre, :release_date, :score, presence: true
-  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :score, numericality: { only_integer: false, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 
   scope :action, -> { where(genre: 'Action') }
   scope :drama, -> { where(genre: 'Drama') }
